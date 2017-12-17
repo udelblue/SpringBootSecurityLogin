a5f32d172b6bc0e3eb4bccfef8227f347effe0af
udelblue <cdsommers@gmail.com>
2017-12-17 16:39:48 -0500

    add fonts

--- src/main/java/com/example/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/configuration/SecurityConfiguration.java
@@ -49,7 +49,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
+		web.ignoring().antMatchers("/resources/**", "/static/**", "/css/**", "/fonts/**", "/js/**", "/images/**");
 	}
 
 }
